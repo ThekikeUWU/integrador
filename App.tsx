fe999a624c99a6db50cbc8a674f71ed0794e96e9
ThekikeUWU <87160164+ThekikeUWU@users.noreply.github.com>
2023-06-06 19:36:24 -0600

    1 avance

---
+++ App.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+
+
+  return (
+    <SafeAreaView>
+    <Text>Hola mundo</Text>
+    </SafeAreaView>
+  );
+};
+export default App;
